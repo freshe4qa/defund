b1c4c0656e203acd00029e3860d26ab0bc1bc2eb
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2023-04-20 11:38:37 +0300

    Update defund4.sh

--- defund4.sh
+++ defund4.sh
@@ -58,14 +58,13 @@ sudo apt install curl build-essential git wget jq make gcc tmux chrony lz4 unzip
 
 # install go
 if ! [ -x "$(command -v go)" ]; then
-ver="1.18.2"
 cd $HOME
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
-source ~/.bash_profile
+wget -O go1.19.1.linux-amd64.tar.gz https://golang.org/dl/go1.19.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.1.linux-amd64.tar.gz && rm go1.19.1.linux-amd64.tar.gz
+echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
+echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
+echo 'export GO111MODULE=on' >> $HOME/.bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
 fi
 
 # download binary
@@ -73,7 +72,8 @@ cd $HOME && rm -rf defund
 git clone https://github.com/defund-labs/defund.git
 cd defund
 git checkout v0.2.6
-make install
+make build
+sudo mv ./build/defundd /usr/local/bin/
 
 # config
 defundd config chain-id $DEFUND_CHAIN_ID
